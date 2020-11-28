44acf45534aa0423c19dc9eb00a6ba1d945cd014
RZDN <nider.rzd@gmail.com>
2020-11-28 10:57:26 -0500

    Actualizacion

--- src/app/pages/profile-consultant/profile-consultant.component.ts
+++ src/app/pages/profile-consultant/profile-consultant.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute} from '@angular/router';
 import { HttpClient} from '@angular/common/http';
+import {Consultant} from '../../model/consultant';
 
 @Component({
   selector: 'app-profile-consultant',
@@ -10,7 +11,7 @@ import { HttpClient} from '@angular/common/http';
 export class ProfileConsultantComponent implements OnInit {
 
   id = 0;
-  sourceConsultant: {};
+  sourceConsultant = new Consultant();
   constructor(private ruta: ActivatedRoute, private http: HttpClient) {
     this.ruta.params.subscribe(param => {
       this.id = param.id;
@@ -20,8 +21,8 @@ export class ProfileConsultantComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.http.get(`https://restcodewebapplication.azurewebsites.net/api/consultants/${this.id}`)
-      .subscribe((data: any[]) => this.sourceConsultant = data);
+    this.http.get(`https://cors-anywhere.herokuapp.com/https://protected-hollows-40842.herokuapp.com/api/consultants/${this.id}`)
+      .subscribe((data: any) => this.sourceConsultant = data);
   }
 
 }
--- src/app/pages/search-consultants/search-consultants.component.ts
+++ src/app/pages/search-consultants/search-consultants.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 
+
 @Component({
   selector: 'app-search-consultants',
   templateUrl: './search-consultants.component.html',
@@ -10,15 +11,14 @@ export class SearchConsultantsComponent implements OnInit {
 
   p = 1;
   filtroValor = '';
+  source: [];
   sourcesRestaurants: Array<any>;
   constructor(private http: HttpClient) { }
-
   handleSearch(value: string): void {
     this.filtroValor = value;
   }
   ngOnInit(): void {
     this.http.get('https://restcodewebapplication.azurewebsites.net/api/consultants')
-      .subscribe((data: any[]) => this.sourcesRestaurants = data);
+      .subscribe((data: [any]) => {this.sourcesRestaurants = data; });
   }
-
 }
